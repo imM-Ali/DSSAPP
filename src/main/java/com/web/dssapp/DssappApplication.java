b530d543b1615e668e25bcd9e7c523459a0be0cf
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2023-04-20 10:16:37 +0100

    Initialized project

---
+++ src/main/java/com/web/dssapp/DssappApplication.java
@@ -0,0 +1,13 @@
+package com.web.dssapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssappApplication.class, args);
+	}
+
+}
