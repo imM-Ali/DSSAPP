c4a91e2a61d588097e47c5c406f85730309c8f62
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2023-05-02 22:39:20 +0100

    Added different views depending on the user type

--- src/main/java/com/web/dssapp/controller/GlobalControllerAdvice.java
+++ src/main/java/com/web/dssapp/controller/GlobalControllerAdvice.java
@@ -1,32 +1,37 @@
 package com.web.dssapp.controller;
 
-import java.security.Principal;
 import java.util.Collection;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.web.dssapp.config.CustomUserDetails;
-import com.web.dssapp.model.Role;
 import com.web.dssapp.model.User;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
-    @ModelAttribute
-    public void addLoggedInUser(Model model, RedirectAttributes atr) {
-    	//UserDetails userDetails = (UserDetails)SecurityContextHolder.getContext().getAuthentication(); 
-    	if( !(SecurityContextHolder.getContext().getAuthentication().getPrincipal()=="anonymousUser")) {    	
+	@ModelAttribute
+	public void addLoggedInUser(Model model, RedirectAttributes atr) {
+		// UserDetails userDetails =
+		// (UserDetails)SecurityContextHolder.getContext().getAuthentication();
+		if (!(SecurityContextHolder.getContext().getAuthentication().getPrincipal() == "anonymousUser")) {
+			Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication()
+					.getAuthorities();
+			for (GrantedAuthority authority : authorities) {
+				model.addAttribute("currentaccess", authority.toString());
+				
+			}
+			User authentication = ((CustomUserDetails) SecurityContextHolder.getContext().getAuthentication()
+					.getPrincipal()).getContext();
+			if (authentication != null) {
+				model.addAttribute("currentuser", authentication);
+			}
+		}
 
-    		User authentication = ((CustomUserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getContext();
-    		if (authentication != null) { model.addAttribute("currentuser",  authentication); }
-    	}
-		
-    }
+	}
 }
