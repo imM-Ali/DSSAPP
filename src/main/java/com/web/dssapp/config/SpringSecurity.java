ab0cb2aa503cbb05b2a0ff2e7931f109996e0268
Muhammad Ali Shahzaib <72030025+imM-Ali@users.noreply.github.com>
2023-05-04 14:01:00 +0100

    Update SpringSecurity.java

--- src/main/java/com/web/dssapp/config/SpringSecurity.java
+++ src/main/java/com/web/dssapp/config/SpringSecurity.java
@@ -44,7 +44,7 @@ public class SpringSecurity {
 	                authz.requestMatchers("/signup","/login").permitAll()
 	                .requestMatchers("/roles").hasAuthority("admin")
 	                .requestMatchers("users/**").hasAuthority("admin")
-					.requestMatchers("/editmovie/**").hasAnyAuthority("admin")
+					.requestMatchers("/editmovie/**").hasAuthority("admin")
 					.requestMatchers("/deletemovie/**").hasAuthority("admin")
 					.requestMatchers("/users/**").hasAuthority("admin")
 					.requestMatchers("roles").hasAuthority("admin")
